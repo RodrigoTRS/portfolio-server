03bfe4449c3db3b1561d38bc6aabbbe2b3eefd30
RodrigoTRS <rodrigoteix1998@gmail.com>
2024-03-07 16:51:19 -0300

    finishing create and fetch languages functionalities

--- src/http/controllers/languages/routes.ts
+++ src/http/controllers/languages/routes.ts
@@ -1,8 +1,8 @@
 import { FastifyInstance } from "fastify";
-import { fetchProjects } from "../projects/fetch-projects";
 import { create } from "./create-language";
+import { fetchLanguages } from "./fetch-languages";
 
 export async function languagesRouter(app: FastifyInstance) {
-  app.get("/languages", fetchProjects);
+  app.get("/languages", fetchLanguages);
   app.post("/languages", create);
 }
